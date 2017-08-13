00123042cf775b4c4b2eb28bd7b043056cad9057
Keerthi Nelaturu <Keerthi_Nelaturu@avivacanada.com>
2017-08-13 01:15:28 -0400

    Adding expense component

--- demo-ui/src/main/ui/src/app/app.module.ts
+++ demo-ui/src/main/ui/src/app/app.module.ts
@@ -2,17 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AddExpenseComponent } from './expense/add-expense/add-expense.component';
+import { ViewExpenseComponent } from './expense/view-expense/view-expense.component';
+import { ExpenseComponent } from './expense/expense.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'expense',
+    component: ExpenseComponent
+  },
+  { path: '',
+    redirectTo: '/expense',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddExpenseComponent,
+    ViewExpenseComponent,
+    ExpenseComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
